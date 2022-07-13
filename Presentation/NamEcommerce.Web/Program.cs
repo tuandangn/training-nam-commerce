792a6b6148370e5d15e26ebd88c4bd40e77208a9
Tuấn Đăng <nguyentuandang7@gmail.com>
2022-07-13 13:19:28 +0700

    Add project files.

---
+++ Presentation/NamEcommerce.Web/Program.cs
@@ -0,0 +1,17 @@
+namespace NamEcommerce.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+            app.UseRouting();
+            app.MapDefaultControllerRoute();
+
+            app.Run();
+        }
+    }
+}
